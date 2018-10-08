3a08b4863d45c695f5acf111c2191d98bf945dbd
Dominic Tremblay <dominictremblay24@gmail.com>
2018-10-08 15:15:35 -0400

    demo2: added the static html article

---
+++ demo2/pets.js
@@ -0,0 +1,22 @@
+const pets = [
+  {
+    name: 'Dingo',
+    type: 'dog',
+    age: 1,
+  },
+  {
+    name: 'Kongo',
+    type: 'snake',
+    age: 3,
+  },
+  {
+    name: 'Meowngo',
+    type: 'cat',
+    age: 2,
+  },
+  {
+    name: 'Bongo',
+    type: 'dog',
+    age: 2,
+  },
+]
